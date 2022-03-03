95ba2a5f7c59029a12e73e570e2106012866bbe0
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-03-02 20:27:24 -0800

    add the requested longURL and its generated shortURL to urlDatabase

--- express_server.js
+++ express_server.js
@@ -30,14 +30,17 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
 app.get("/urls/:shortURL", (req,res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars)
+});
+
+app.post("/urls", (req, res) => {
+  
+  console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`) 
 });
 
 app.get("/hello", (req, res) => {
