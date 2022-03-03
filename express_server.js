956b0dd6b4531c9e21a99cbf6a457ad473f5fb72
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-03-02 16:25:47 -0800

    request and use body-parser

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,7 +31,6 @@ app.get("/urls/:shortURL", (req,res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars)
 });
-
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
