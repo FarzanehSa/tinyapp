3f12fa44ee5fc82d4d2c9f2475b219d4a849f75e
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-03-06 16:47:46 -0800

    check the authorization for login and set cookie

--- express_server.js
+++ express_server.js
@@ -19,19 +19,20 @@ const urlDatabase = {
 };
 
 // startpoint userDB 🟣
-const users = { 
-  //   "userRandomID": {
-    //     id: "userRandomID", 
-    //     email: "user@example.com", 
-    //     password: "purple-monkey-dinosaur"
-    //   },
-    //  "user2RandomID": {
-      //     id: "user2RandomID", 
-      //     email: "user2@example.com", 
-      //     password: "dishwasher-funk"
-      //   }
+const users = {
+  //  "userRandomID": {
+  //    id: "userRandomID",
+  //    email: "user@example.com",
+  //    password: "purple-monkey-dinosaur"
+  //  },
+  //  "user2RandomID": {
+  //    id: "user2RandomID",
+  //    email: "user2@example.com",
+  //    password: "dishwasher-funk"
+  //  }
 };
-    
+
+// error Database 🟣
 const errors = {
   e1: {
     code: 404,
@@ -50,7 +51,19 @@ const errors = {
     h3: "Email is already registered",
     h5: "Please login with your email or register new email.",
     image: "error"
-  }
+  },
+  e4: {
+    code: 403,
+    h3: "Invalid login",
+    h5: "No user with that email!",
+    image: "error"
+  },
+  e5: {
+    code: 403,
+    h3: "Incorrect Password",
+    h5: "Try again!",
+    image: "error"
+  },
 };
 
 // generating random alphanumeric length 6 for shortURL & userID 🟣
@@ -58,10 +71,10 @@ const generateRandomString = function() {
   return Math.random().toString(36).slice(2,8);
 };
 
-// check eamil already exict in userDB or not 🟠
+// check if eamil already exist in userDB, return userID or false 🟣
 const getUserByEmail = function(userDB, userEmail) {
   for (const user in userDB) {
-    if (users[user].email === userEmail) return true;
+    if (users[user].email === userEmail) return users[user].id;
   }
   return false;
 };
@@ -79,18 +92,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
     
-// render registration template 🟠 ❓ pass user
+// render registration template 🟣
 app.get("/register", (req, res) => {
   const templateVars = {
-    user: users[req.cookies.user_id]
+    user: undefined
   };
   res.render("registeration", templateVars);
 });
 
-// render login template 🟠 ❓ pass user
+// render login template 🟣
 app.get("/login", (req, res) => {
   const templateVars = {
-    user: users[req.cookies.user_id]
+    user: undefined
   };
   res.render("login", templateVars);
 });
@@ -124,6 +137,11 @@ app.get("/urls/:shortURL", (req,res) => {
   }
 });
 
+// link on the shortURL will redirect to it's longURL path 🟣
+app.get("/u/:shortURL", (req,res) => {
+  res.redirect(`${urlDatabase[req.params.shortURL]}`);
+});
+
 // get longURL from form in new template, generate shortURL and add them in urlDB  then redirect 🟣
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
@@ -131,25 +149,22 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-// link on the shortURL will redirect to it's longURL path 🟣
-app.get("/u/:shortURL", (req,res) => {
-  res.redirect(`${urlDatabase[req.params.shortURL]}`);
-});
-
-// get email & password from form in regestration template 🟠
+// get email & password from form in regestration template 🟣
 app.post("/register", (req,res) => {
+  const newEmail = req.body.email;
+  const newPass = req.body.password;
   // If email/password are empty, send back response with 400 status code
-  if (req.body.email.length === 0 || req.body.password.length === 0) {
+  if (newEmail.length === 0 || newPass.length === 0) {
     const templateVars = {
-      user: users[req.cookies.user_id],
+      user: undefined,
       error: errors.e2
     };
     res.statusCode = errors.e2.code;
     res.render("error", templateVars);
     // If email already registerd, send response back with 400 status code
-  } else if (getUserByEmail(users, req.body.email)) {
+  } else if (getUserByEmail(users, newEmail)) {
     const templateVars = {
-      user: users[req.cookies.user_id],
+      user: undefined,
       error: errors.e3
     };
     res.statusCode = errors.e3.code;
@@ -159,11 +174,10 @@ app.post("/register", (req,res) => {
     const userID = generateRandomString();
     users[userID] = {
       id: userID,
-      password: req.body.password,
-      email: req.body.email,
-    }
+      email: newEmail,
+      password: newPass,
+    };
     res.cookie('user_id',userID);
-    // console.log(users); // 🚨
     res.redirect("/urls");
   }
 });
@@ -180,15 +194,46 @@ app.post("/urls/:id", (req,res) => {
   res.redirect("/urls");
 });
 
-// set cookie w/ username 🟢🟠
+// login check 3 false situation, blank input or email not exist or password not match otherwise set cookie. 🟣
 app.post("/login", (req, res) => {
-  // res.cookie('username', req.body.username);
-  res.redirect("/urls");
+  const curEmail = req.body.email;
+  const curPass = req.body.password;
+  // If email/password are empty, send back response with 400 status code
+  if (curEmail.length === 0 || curPass.length === 0) {
+    const templateVars = {
+      user: undefined,
+      error: errors.e2
+    };
+    res.statusCode = errors.e2.code;
+    res.render("error", templateVars);
+  // if email is not exist, send back response with 403 status code
+  } else if (!getUserByEmail(users, curEmail)) {
+    const templateVars = {
+      user: undefined,
+      error: errors.e4
+    };
+    res.statusCode = errors.e4.code;
+    res.render("error", templateVars);
+  } else {
+    const userID = getUserByEmail(users, curEmail);
+    // if password does not match, send back response with 403 status code
+    if (users[userID].password !== curPass) {
+      const templateVars = {
+        user: undefined,
+        error: errors.e5
+      };
+      res.statusCode = errors.e5.code;
+      res.render("error", templateVars);
+    } else {
+      // set cookie and redirect
+      res.cookie('user_id', userID);
+      res.redirect("/urls");
+    }
+  }
 });
 
-// clear cookie 🟢🟠 ❓ clear or not?
+// clear cookie 🟣
 app.post("/logout", (req, res) => {
-  // res.clearCookie('username');
   res.clearCookie('user_id');
   res.redirect("/urls");
 });
