d171b81c14aa1ed653c802530e06a30395c3a218
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-03-08 15:54:29 -0800

    add test codes & install mocha & chai

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 // check if eamil already exist in userDB, return userID or false ⚪️
-const getUserByEmail = function(userDB, userEmail) {
+const getUserByEmail = function(userEmail, userDB) {
   for (const user in userDB) {
     if (userDB[user].email === userEmail) return userDB[user];
   }
